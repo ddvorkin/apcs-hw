e67b244375cb1962a41607491ff9e5a59231abc3
David Dvorkin <davidfreddvorkin@gmail.com>
2014-03-13 08:40:53 -0400

    2

--- 07-QuickSort/QuickSort2.java
+++ 07-QuickSort/QuickSort2.java
@@ -2,8 +2,9 @@ public class QuickSort2 {
 
     public int partition (int []a, int L, int R){
 
-	private int p = nextInt(L);
+	private int p = a[nextInt(a.length)];
+	private int si = 0;
 
 	for (int i = L; i < R - 1; i++){
 
-	    
+	    if (i < p)
