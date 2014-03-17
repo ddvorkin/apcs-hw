3ab50b72f9065d8727c33f4a070efafd1bdfd4b2
David <davidfreddvorkin@gmail.com>
2014-03-17 01:49:25 -0400

    Homework #7b

---
+++ 07-QuickSort/QuickSort2.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Qsort2{
+    private Random rnd = new Random();
+
+    public int partition(int[] a, int l, int r) {
+	int tmp;
+	int pIndex = l+rnd.nextInt(r-l);
+	int p = a[pIndex];
+	tmp = a[r];
+	a[r] = a[pIndex];
+	a[pIndex]=tmp;
+
+	int swapindex=l;
+	for (int i=l;i<r;i++) {
+	    if (a[i]<p) {
+		tmp = a[i];
+		a[i]=a[swapindex];
+		a[swapindex]=tmp;
+		swapindex++;
+	    }
+	}
+	tmp = a[swapindex];
+	a[swapindex]=a[r];
+	a[r]=tmp;
+	return swapindex;
+    }
+    
+    public void qsort(int[] a, int l, int r) {
+	if (r-l <= 1)
+	    return;
+	else {
+	    int pi = partition(a,l,r);
+	    if (pi-1>l)
+		qsort(a,l,pi-1);
+	    if (pi+1<r)
+		qsort(a,pi+1,r);
+
+	}
+    }
+    public void  qsort(int[] a) {
+	qsort(a,0,a.length-1);
+    }
+
+
+    public static void main(String[] args) {
+	int n = Integer.parseInt(args[0]);
+	Random rnd = new Random();
+	int[] a = new int[n];
+	for (int i=0;i<n;i++)
+	    a[i]=rnd.nextInt(100);
+	System.out.println(Arrays.toString(a));
+	Qsort2 q = new Qsort2();
+	q.qsort(a);
+	int p = q.partition(a,0,a.length-1);
+	System.out.println("a["+p+"] = "+a[p]);	
+	System.out.println(Arrays.toString(a));
+    }
+    
+}   
