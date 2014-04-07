a5157827bf48f1de755c81573c0e785b30aecfa6
David Dvorkin <davidfreddvorkin@gmail.com>
2014-04-07 08:41:22 -0400

    classwork

---
+++ 13-Calculator/Calculator.java
@@ -0,0 +1,51 @@
+public class Calculator {
+
+    private Node top;
+    private int numElts;
+
+    //constructor 1
+    public MyStack(){
+	top = null;
+    }
+
+    //push 2
+    public void push(String s){
+	Node n = new Node (s);
+	n.setNext(top);
+	top = n;
+	numElts = numElts+1;
+    }
+
+    //pop
+    public String pop(){
+	String s;
+	s = top.getData();
+	top = top.getNext();
+	numElts = numElts - 1;
+	return s;   
+}
+
+    //peek
+    public String peek(){
+	return top.getData();
+    }
+
+    //isEmpty
+    public boolean isEmpty(){
+	return top == null;
+    }
+
+    //toString 3
+    public String toString(){
+	String tmp = "";
+	Node currentNode = top;
+	while (currentNode != null){
+	    tmp += currentNode.getData() + ",";
+	    currentNode = currentNode.getNext();
+	}
+	return tmp;
+    }
+}
+
+/* For string to double: Double.parseDouble(_)
+For string to int: Integer.parseInt(_)
---
+++ 13-Calculator/Driver.java
@@ -0,0 +1,5 @@
+public class Driver{
+    public static void main (String[] args){
+
+Scanner sc = new Scanner(System.in);
+     int i = sc.nextInt();
